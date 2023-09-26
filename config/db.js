348d1acc65f40cd06f6f1ed9959213d6288e3347
Damar Sierra <damarsierra@gmail.com>
2023-09-26 19:04:59 -0400

    configured db_conn

--- config/db.js
+++ config/db.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
-const config = require('config');
-const db = config.get('mongoURI');
+require('dotenv').config()
+const db = process.env.DB_CONN;
+
+//   "mongoURI": "mongodb://localhost:27017/booklist",
 
 const connectDB = async () => {
   try {
@@ -9,7 +11,7 @@ const connectDB = async () => {
       useNewUrlParser: true,
     });
 
-    console.log('MongoDB is Connected...');
+    console.log('MongoDB is Connected...' + db);
   } catch (err) {
     console.error(err.message);
     process.exit(1);
